7ce4bd0f4d91c17682f386563c22ccd9174b338b
Delweng Zheng <delweng@gmail.com>
2019-02-25 17:18:45 +0800

    feat(trace): add 'error' column

--- src/main/java/org/tron/common/runtime/vm/program/Program.java
+++ src/main/java/org/tron/common/runtime/vm/program/Program.java
@@ -581,7 +581,7 @@ public class Program {
           Hex.toHexString(newAddress),
           createResult.getException());
 
-      internalTx.reject();
+      internalTx.reject(createResult.getException(), createResult.isRevert());
       createResult.rejectInternalTransactions();
 
       stackPushZero();
@@ -776,7 +776,7 @@ public class Program {
             Hex.toHexString(contextAddress),
             callResult.getException());
 
-        internalTx.reject();
+        internalTx.reject(callResult.getException(), callResult.isRevert());
         callResult.rejectInternalTransactions();
 
         stackPushZero();
--- src/main/java/org/tron/core/capsule/TransactionInfoCapsule.java
+++ src/main/java/org/tron/core/capsule/TransactionInfoCapsule.java
@@ -231,6 +231,7 @@ public class TransactionInfoCapsule implements ProtoCapsule<TransactionInfo> {
         internalTrxBuilder.setOutput(ByteString.copyFrom(internalTransaction.getOutput()));
         internalTrxBuilder.setFeeLimit(internalTransaction.getFeeLimit());
         internalTrxBuilder.setFeeUsed(internalTransaction.getFeeUsed());
+        internalTrxBuilder.setError(ByteString.copyFrom(internalTransaction.getError().getBytes()));
 
         builder.addInternalTransactions(internalTrxBuilder);
       }
